3c9282ba7be9c1b850a47130a74d7cdd5348d796
mohammed-eddaghal <mohammed_eddaghal@um5.ac.ma>
2021-12-04 15:35:46 +0100

    add architecture service

---
+++ src/main/java/com/twitter/homeTimeLine/api/model/TweetAPI.java
@@ -0,0 +1,11 @@
+package com.twitter.homeTimeLine.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TweetAPI {
+}
