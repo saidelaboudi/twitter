5ae8ee93a1998388197351f97f3f8b5eceb0b11a
blankXI <56233799+SefiatAhmedAmine@users.noreply.github.com>
2021-12-07 20:02:26 +0100

    update continuation

--- src/main/java/com/twitter/userTimeLineService/domain/port/api/IUserTimeLineApiPort.java
+++ src/main/java/com/twitter/userTimeLineService/domain/port/api/IUserTimeLineApiPort.java
@@ -2,6 +2,6 @@ package com.twitter.userTimeLineService.domain.port.api;
 
 import com.twitter.tweet.domain.port.api.ITweetPortToApi;
 
-public interface IUserTimeLineApiPort extends ITweetPortToApi {
+public interface IUserTimeLineApiPort {
 
 }
