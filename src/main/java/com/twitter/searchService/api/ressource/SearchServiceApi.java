dacb47976bad89a53abe83806783d6a8e90f4f88
SAID El ABOUDI <saidelaboudi3@gmail.com>
2021-12-07 20:03:40 +0100

    LGTM

--- src/main/java/com/twitter/searchService/api/ressource/SearchServiceApi.java
+++ src/main/java/com/twitter/searchService/api/ressource/SearchServiceApi.java
@@ -2,29 +2,28 @@ package com.twitter.searchService.api.ressource;
 
 import com.twitter.searchService.domain.port.api.ISearchServiceApi;
 import com.twitter.socialGraph.api.model.UserAPI;
+import com.twitter.socialGraph.domain.model.UserDomain;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/v1/api/SearchService")
+@RequestMapping("/v1/api/search")
 public class SearchServiceApi {
     private ISearchServiceApi searchService;
 
-    @GetMapping("/user/{user}")
-    public List<UserAPI> searchByUser(@PathVariable  String user){
-        searchService.searchByUser(user);
-        return null;
+    @GetMapping("/users/{username}")
+    public List<UserAPI> searchByUsername(@PathVariable  String username){
+        return searchService.searchByUsername(username).stream().map(UserDomain::toAPI).collect(Collectors.toList());
     }
 
-    @GetMapping("/article/{article}")
+    @GetMapping("/articles/{article}")
     public List<UserAPI> searchByArticle(@PathVariable String article){
-        searchService.searchByArticle(article);
-        return null;
+        return searchService.searchByArticle(article).stream().map(UserDomain::toAPI).collect(Collectors.toList());
     }
-    @GetMapping("/keyword/{Keyword}")
+    @GetMapping("/users/keywords/{keyword}")
     public List<UserAPI> searchByKeyword(@PathVariable String keyword){
-        searchService.searchByKeyword(keyword);
-        return null;
+        return searchService.searchByKeyword(keyword).stream().map(UserDomain::toAPI).collect(Collectors.toList());
     }
 }
--- src/main/java/com/twitter/searchService/domain/port/api/ISearchServiceApi.java
+++ src/main/java/com/twitter/searchService/domain/port/api/ISearchServiceApi.java
@@ -6,7 +6,7 @@ import com.twitter.socialGraph.domain.model.UserDomain;
 import java.util.List;
 
 public interface ISearchServiceApi {
-    public List<UserDomain> searchByUser(String user);
+    public List<UserDomain> searchByUsername(String user);
     public List<UserDomain> searchByArticle(String article);
     public List<UserDomain> searchByKeyword(String keyword);
 }
